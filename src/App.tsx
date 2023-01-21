7585d48c15d25e152f7ddb9da1fcdfe4cc946ac2
feddorovich <fedsygreen@mail.ru>
2023-01-21 16:38:00 +0300

    lesson 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies";
 
 const App = () => {
     return (
         <div>
             <Header/>
             <Technologies/>
-        </div>
-    );
-}
-
-const Technologies = () => {
-    return (
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>reack</li>
-        </ul>
-    );
-}
-
-const Header = () => {
-    return (
-        <div>
-            <a href="#">Home</a>
-            <a href="#">News Feed</a>
-            <a href="#">Messages</a>
         </div>
     );
 }
