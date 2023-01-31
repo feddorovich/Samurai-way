20eb2901e8f9128aa956abf40f6b6093e0e83af3
feddorovich <feddorovich@outlook.com>
2023-01-31 22:22:56 +0300

    lesson 14

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f2b060c7a92408bac811_logo.svg"
                  alt=""/>
         </header>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 function Profile() {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src="https://sletaem.kz/upload/iblock/bb9/bb92209a394938a93de111fcf86de120.jpg" alt=""/>
             </div>
             <div>
                 ava + desctiption
             </div>
-            <div>
-                My posts
-                <div>New post</div>
-                <div>Post 1</div>
-                <div>Post 2</div>
+            <div className={s.posts}>
+                <div className={s.item}>
+                    My posts
+                </div>
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
             </div>
         </div>
     )
